f42d2def77aef0c36b84d262c90cffabe7f3a6d1
Ariel G <ariel.garces@uisek.edu.ec>
2025-10-29 12:38:13 -0500

    Get con Retrofit
    Operaciones de GET para retrofit agregadas.

--- app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/MainActivity.kt
@@ -1,13 +1,18 @@
 package ec.edu.uisek.githubclient
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
-import ec.edu.uisek.githubclient.databinding.FragmentRepoItemBinding
 
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+import ec.edu.uisek.githubclient.databinding.ActivityMainBinding
+import ec.edu.uisek.githubclient.models.Repo
+import ec.edu.uisek.githubclient.services.GithubApiService
+import ec.edu.uisek.githubclient.services.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var reposAdapter: ReposAdapter
@@ -19,10 +24,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupRecyclerView()
+        fetchRepositorios()
     }
 
     private fun setupRecyclerView() {
         reposAdapter = ReposAdapter()
         binding.reposRecyclerView.adapter = reposAdapter
+    }
+
+    private fun fetchRepositorios(){
+        val apiService: GithubApiService= RetrofitClient.gitHubApiService
+        val call = apiService.getRepos()
+        call.enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                if(response.isSuccessful){
+                    val repos = response.body()
+                    if (repos != null && repos.isNotEmpty()){
+                    reposAdapter.updateRepositorios(repos)
+                    } else {
+                        showMessage("No se encontraron repositorios")
+                    }
+                }else{
+                    val errorMessage = when(response.code()){
+                        401 -> "No autorizado"
+                        403 -> "Prohibido"
+                        404 -> "No encontrado"
+                        else -> "Error ${response.code()}"
+                    }
+                    showMessage("Error: $errorMessage")
+
+                }
+
+
+            }
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                showMessage("No se pudieron cargar lo repositorios")
+
+            }
+        })
+    }
+    private fun showMessage (message:String){
+        Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/ec/edu/uisek/githubclient/ReposAdapter.kt
+++ app/src/main/java/ec/edu/uisek/githubclient/ReposAdapter.kt
@@ -4,18 +4,28 @@ package ec.edu.uisek.githubclient
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import ec.edu.uisek.githubclient.databinding.FragmentRepoItemBinding
+import ec.edu.uisek.githubclient.models.Repo
 
 class ReposViewHolder(private val binding: FragmentRepoItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
-    fun bind(position: Int) {
-        binding.repoName.text = "Repositorio No. ${position}"
-        binding.repoDescription.text = "Esta es la descripción para el elmento No. ${position}"
+    fun bind(repo: Repo) {
+        binding.repoName.text = repo.name
+        binding.repoDescription.text = repo.description
+        binding.repoLang.text = repo.language
+        Glide.with(binding.root.context)
+            .load(repo.owner.avatarUrl)
+            .placeholder(R.mipmap.ic_launcher)
+            .error(R.mipmap.ic_launcher)
+            .circleCrop()
+            .into(binding.reportOwnerImage)
     }
 }
 
 class ReposAdapter: RecyclerView.Adapter<ReposViewHolder>() {
-    override fun getItemCount(): Int = 3
+    private var repositorios: List<Repo> = emptyList()
+    override fun getItemCount(): Int = repositorios.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReposViewHolder {
         var binding = FragmentRepoItemBinding.inflate(
             LayoutInflater.from(parent.context),
@@ -26,6 +36,11 @@ class ReposAdapter: RecyclerView.Adapter<ReposViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ReposViewHolder, position: Int) {
-        holder.bind(position)
+        holder.bind(repositorios[position])
+
+    }
+    fun updateRepositorios(newRepositorios: List<Repo>) {
+        repositorios = newRepositorios
+        notifyDataSetChanged()
     }
 }
